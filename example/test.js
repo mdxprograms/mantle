6df0a7cb4aa66cb537f8b0833d1896297966374a
Josh Waller <mdx.programs@gmail.com>
2020-03-26 13:00:33 -0400

    more experimenting

--- example/test.js
+++ example/test.js
@@ -4,36 +4,54 @@ const { article, main, div, button, input, ul, li, span } = DOM;
 
 const Root = () => document.getElementById("app");
 
-const update = () => Root().replaceWith(App(data));
+const update = () =>
+  Root()
+    .querySelector("#app-root")
+    .replaceWith(App(state));
 
-let data = {
-  person: "",
-  people: [{ name: "Josh" }, { name: "Annie" }]
+const state = {};
+Object.defineProperties(state, {
+  person: {
+    value: "",
+    writable: true,
+    configurable: true,
+    enumerable: true
+  },
+  people: {
+    value: [],
+    writable: true,
+    configurable: true,
+    enumerable: true
+  }
+});
+
+const handlePerson = e => {
+  state.person = e.target.value;
 };
+const personInput = person => input({ onkeyup: handlePerson }, person);
 
-const personInput = person =>
-  input({ onkeyup: e => (data.person = e.target.value) }, person);
-
+const handleAddPerson = () => {
+  state.people.push({ name: state.person });
+  update();
+};
 const addPersonBtn = button(
   {
-    onclick: () => {
-      data.people.push({ name: data.person });
-      data.person = "";
-      console.log(this)
-      update();
-    }
+    onclick: handleAddPerson
   },
   "Add"
 );
 
-const removePersonBtn = name =>
+const handleRemovePerson = e => {
+  state.people = state.people.filter(
+    p => p.name !== e.target.parentElement.querySelector("span").textContent
+  );
+  update();
+};
+const removePersonBtn = () =>
   button(
     {
       className: "f6 link dim br1 ph3 pv2 dib black bg-dark-red pointer",
-      onclick: e => {
-        data.people = data.people.filter(p => p.name !== name);
-        update();
-      }
+      onclick: handleRemovePerson
     },
     "Delete?"
   );
@@ -42,14 +60,17 @@ const peopleList = people =>
   ul(
     { className: "list pl3 pv3 lw6 ba b--light-silver br2" },
     people.map(p =>
-      li({}, [span({ className: "mr2" }, p.name), removePersonBtn(p.name)])
+      li({}, [span({ className: "mr2" }, p.name), removePersonBtn()])
     )
   );
 
-const App = data =>
-  main({ id: "app" }, [
-    div({}, [personInput(data.person), addPersonBtn]),
-    article({ className: "pa3 pa5-ns" }, [peopleList(data.people)])
+const App = state =>
+  main({ id: "app-root" }, [
+    div({}, [personInput(state.person), addPersonBtn]),
+    article(
+      { className: "pa3 pa5-ns" },
+      state.people.length > 0 ? [peopleList(state.people)] : []
+    )
   ]);
 
-mount(Root(), App(data));
+mount(Root(), App(state));
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import DOM from "./dom";
 
-const createTreeItem = el => ({
-  mId: el.mId,
-  parentId: el.parentNode.mId,
-  order: Array.from(el.parentNode.childNodes).indexOf(el),
-  type: el.localName,
-  attributes: [...el.attributes].map(attr => ({
-    key: attr.localName,
-    value: attr.value
-  }))
-});
+// const createTreeItem = el => ({
+//   mId: el.mId,
+//   parentId: el.parentNode.mId,
+//   order: Array.from(el.parentNode.childNodes).indexOf(el),
+//   type: el.localName,
+//   attributes: [...el.attributes].map(attr => ({
+//     key: attr.localName,
+//     value: attr.value
+//   }))
+// });
 
-const createTree = container =>
-  [...container.querySelectorAll("*")].map(createTreeItem);
+// const createTree = container =>
+//   [...container.querySelectorAll("*")].map(createTreeItem);
 
 const mount = (el, container) => {
   el.appendChild(container);
-  const tree = createTree(el);
-  console.log(tree);
+  // const tree = createTree(el);
 };
 
 export { DOM, mount };
