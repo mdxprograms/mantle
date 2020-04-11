233759647228d836be7ba26c91807a4816fd13b5
Josh Waller <mdx.programs@gmail.com>
2020-04-11 01:12:12 -0400

    Adding possible state class option

--- example/test.js
+++ example/test.js
@@ -1,76 +1,62 @@
-import { DOM, mount } from "../src";
+import { DOM, mount, State } from "../src";
 
 const { article, main, div, button, input, ul, li, span } = DOM;
 
 const Root = () => document.getElementById("app");
 
-const update = () =>
-  Root()
-    .querySelector("#app-root")
-    .replaceWith(App(state));
+const update = () => Root().querySelector("#app-root").replaceWith(App(state));
 
-const state = {};
-Object.defineProperties(state, {
-  person: {
-    value: "",
-    writable: true,
-    configurable: true,
-    enumerable: true
-  },
-  people: {
-    value: [],
-    writable: true,
-    configurable: true,
-    enumerable: true
-  }
+const state = new State({
+  person: "",
+  people: [],
 });
 
-const handlePerson = e => {
-  state.person = e.target.value;
-};
-const personInput = person => input({ onkeyup: handlePerson }, person);
+const handlePerson = (e) => state.update("person", e.target.value);
+const personInput = (person) => input({ onkeyup: handlePerson }, person);
 
-const handleAddPerson = () => {
-  state.people.push({ name: state.person });
-  update();
-};
+const handleAddPerson = () =>
+  state.update("people", state.get("people").concat(state.get("person")));
 const addPersonBtn = button(
   {
-    onclick: handleAddPerson
+    onclick: handleAddPerson,
   },
   "Add"
 );
 
-const handleRemovePerson = e => {
-  state.people = state.people.filter(
-    p => p.name !== e.target.parentElement.querySelector("span").textContent
+const handleRemovePerson = (e) => {
+  state.update(
+    "people",
+    state.get("people").filter(
+      (p) => p.name !== e.target.parentElement.querySelector("span").textContent
+    )
   );
-  update();
 };
 const removePersonBtn = () =>
   button(
     {
       className: "f6 link dim br1 ph3 pv2 dib black bg-dark-red pointer",
-      onclick: handleRemovePerson
+      onclick: handleRemovePerson,
     },
     "Delete?"
   );
 
-const peopleList = people =>
+const peopleList = (people) =>
   ul(
     { className: "list pl3 pv3 lw6 ba b--light-silver br2" },
-    people.map(p =>
+    people.map((p) =>
       li({}, [span({ className: "mr2" }, p.name), removePersonBtn()])
     )
   );
 
-const App = state =>
+console.log(state)
+
+const App = (state) =>
   main({ id: "app-root" }, [
-    div({}, [personInput(state.person), addPersonBtn]),
+    div({}, [personInput(state.get("person")), addPersonBtn]),
     article(
       { className: "pa3 pa5-ns" },
-      state.people.length > 0 ? [peopleList(state.people)] : []
-    )
+      state.get("people").length > 0 ? [peopleList(state.get("people"))] : []
+    ),
   ]);
 
 mount(Root(), App(state));
--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import DOM from "./dom";
 
-// const createTreeItem = el => ({
-//   mId: el.mId,
-//   parentId: el.parentNode.mId,
-//   order: Array.from(el.parentNode.childNodes).indexOf(el),
-//   type: el.localName,
-//   attributes: [...el.attributes].map(attr => ({
-//     key: attr.localName,
-//     value: attr.value
-//   }))
-// });
+const createTreeItem = el => ({
+  mId: el.mId,
+  parentId: el.parentNode.mId || null,
+  order: Array.from(el.parentNode.childNodes).indexOf(el),
+  type: el.localName,
+  attributes: [...el.attributes].map(attr => ({
+    key: attr.localName,
+    value: attr.value
+  }))
+});
 
-// const createTree = container =>
-//   [...container.querySelectorAll("*")].map(createTreeItem);
+const createTree = container =>
+  [...container.querySelectorAll("*")].map(createTreeItem);
+
+export class State {
+  constructor(initialState = {}) {
+    this.current = initialState;
+  }
+
+  update(statePath, value) {
+    console.log(statePath, value)
+    this.current[statePath] = value;
+  }
+
+  get(key) {
+    return this.current[key];
+  }
+}
 
 const mount = (el, container) => {
+  const tree = createTree(container);
   el.appendChild(container);
-  // const tree = createTree(el);
 };
 
 export { DOM, mount };
