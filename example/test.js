2fd4a865b20b2ad2d28e90e876c04e39215cd47f
Josh <mdx.programs@gmail.com>
2020-05-09 17:18:06 -0400

    Testing list appending and removing

--- example/test.js
+++ example/test.js
@@ -1,21 +1,35 @@
 import { DOM, mount, dispatch } from "../src";
 
-const { main, div, input } = DOM;
+const { main, div, input, button, ul, li } = DOM;
 
 const Root = document.getElementById("app");
 
-const personInput = input(
-  {
-    onkeyup: (e) => dispatch("person:update", e.target.value),
-  },
-  ""
-);
-
-const testOutput = div({
-  "person:update": (val) => (testOutput.textContent = val),
+const personInput = input({
+  onkeyup: (e) => dispatch("person:update", e.target.value),
 });
 
-const container = div({ className: "container" }, [personInput, testOutput]);
+const addPersonBtn = button(
+  {
+    onclick: () => dispatch("person:added", personInput.value),
+  },
+  "Add person"
+);
+
+const personListEvents = {
+  "person:added": (val) =>
+    personList.appendChild(
+      li({ onclick: (e) => dispatch("person:removed", e.target) }, val)
+    ),
+  "person:removed": (li) => li.remove(),
+};
+
+const personList = ul(personListEvents, li({}, "First child"));
+
+const container = div({ className: "container" }, [
+  personInput,
+  addPersonBtn,
+  personList,
+]);
 
 const App = main({ id: "app-root" }, container);
 
