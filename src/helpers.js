d6d78729882ee72ce6e290c70fedfd849d4addf4
Josh Waller <mdx.programs@gmail.com>
2020-06-26 10:09:13 -0400

    Adding initial helpers

---
+++ src/helpers.js
@@ -0,0 +1,16 @@
+export const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x)
+
+export const changeProp = (k, v) => el => {
+  el.setAttribute(k, v)
+  return el
+}
+
+export const getProp = k => el => el.getAttribute(k)
+
+export const setStyle = styles => el => {
+  styles.forEach(([k, v]) => {
+    el.style[k] = v
+  })
+
+  return el
+}
