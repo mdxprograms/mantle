f7513e649736b3aeb4fdad229f65461830014f37
Josh Waller <mdx.programs@gmail.com>
2020-06-26 12:22:40 -0400

    Adds docs for helpers, remove unused code, adds exports

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,55 @@
+/**
+ * @name compose
+ * @description Compose multiple changes on a data source
+ * @example
+ * compose(
+ *   setProp('disabled', true),
+ *   setStyle('background', '#ddd')
+ * )(el)
+ * @param  {Function[]} fns
+ * @param  {any} x
+ * @return {any}
+ */
 export const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x)
 
-export const changeProp = (k, v) => el => {
+/**
+ * @name setProp
+ * @description Set a property's value on a curried element
+ * @example
+ * setProp('disabled', true)(el)
+ * @param  {String} k
+ * @param  {any} v
+ * @param  {HTMLElement} el
+ * @return {HTMLElement}
+ */
+export const setProp = (k, v) => el => {
   el.setAttribute(k, v)
   return el
 }
 
+/**
+ * @name getProp
+ * @description Returns a value for a curried elements property
+ * @example
+ * getProp('src')(el)
+ * @param  {String} k
+ * @param {HTMLElement} el
+ * @return {any}
+ */
 export const getProp = k => el => el.getAttribute(k)
 
+/**
+ * @name setStyle
+ * @description Ability to set inline styles on a curried element
+ * @param  {Array[]} styles
+ * @param  {HTMLElement} el
+ * @example
+ * setStyle([
+ *   ["background", "#f7f7f7"],
+ *   ["color", "#444"]
+ * ])(el)
+ * @return  {HTMLElement}
+ */
 export const setStyle = styles => el => {
   styles.forEach(([k, v]) => {
     el.style[k] = v
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import DOM from './dom'
-import { dispatch, on } from './emitter'
+import { dispatch } from './emitter'
+import { setProp, setStyle, getProp, compose } from './helpers'
 
 const mount = (el, container) => {
   el.appendChild(container)
 }
 
-export { DOM, mount, dispatch, on }
+export { DOM, mount, dispatch, setProp, setStyle, getProp, compose }
