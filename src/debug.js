df6def530bbd685c81aa47db08a3fb15988dbde1
Josh Waller <mdx.programs@gmail.com>
2020-07-06 11:41:16 -0400

    debug updates

--- src/debug.js
+++ src/debug.js
@@ -1,11 +1,11 @@
+
 import { on } from './emitter'
 import DOM from './dom'
 import { append, setStyle } from './helpers'
 
-const { div, p } = DOM
+const { div, ul, li } = DOM
 
-const widget = div({}, [])
-setStyle([
+const widget = setStyle([
   ['background', '#ddd'],
   ['border-top-left-radius', '4px'],
   ['bottom', 0],
@@ -17,11 +17,31 @@ setStyle([
   ['padding', '0 10px'],
   ['position', 'absolute'],
   ['right', 0]
-])(widget)
+])(div({}, []))
+
+const NewEntryValue = (key, val) =>
+  setStyle([
+    ['padding', '5px 8px']
+  ])(li({}, `${key}: ${val}`))
+
+const NewEntry = (evtName, payload, timestamp) =>
+  setStyle([
+    ['background', '#fff'],
+    ['border-radius', '4px'],
+    ['padding', '0 5px 5px 0'],
+    ['list-style', 'none'],
+    ['transition', 'all 200ms ease-in']
+  ])(
+    ul({}, [
+      NewEntryValue('Event', evtName),
+      NewEntryValue('Payload', payload),
+      NewEntryValue('Timestamp', new Date().toLocaleTimeString(timestamp))
+    ])
+  )
 
 const debug = () =>
   on('*', (evtName, payload, timestamp) =>
-    append(p({}, `${evtName} ${payload} ${timestamp}`))(widget)
+    append(NewEntry(evtName, payload, timestamp))(widget)
   )
 
 document.body.appendChild(widget)
