17f0f9dd8954604ea3574d54c0a3bb482713d4b6
Josh Waller <mdx.programs@gmail.com>
2021-05-14 03:22:45 -0400

    emitter refactor init

--- src/emitter.js
+++ src/emitter.js
@@ -1,7 +1,31 @@
 import nanobus from 'nanobus'
 
-const bus = nanobus()
+// const bus = nanobus()
 
-export const dispatch = (evtName, val) => bus.emit(evtName, val)
+// export const dispatch = (evtName, val) => bus.emit(evtName, val)
 
-export const on = (evtName, cb) => bus.on(evtName, cb)
+// export const on = (evtName, cb) => bus.on(evtName, cb)
+
+export let Bus = {}
+
+export const dispatch = (evtName, val) => {
+  if (!Bus[evtName]) {
+    if (String(val)) {
+      Bus = { ...Bus, [`${evtName}`]: () => val }
+    } else {
+      Bus = { ...Bus, [`${evtName}`]: val }
+    }
+  }
+
+  return String(val) ? Bus[evtName](val) : val()
+}
+
+export const on = (evtName, cb) => {
+  if (!Bus[evtName]) {
+    Bus = { ...Bus, [`${evtName}`]: cb }
+    console.log(Bus)
+    return Bus[evtName]
+  }
+
+  return cb
+}
